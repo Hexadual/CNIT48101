da45d540feeb8aa4e3232ebaf372d2a658be34ae
Rocobo <sirjacob2u@gmail.com>
2024-10-16 19:34:30 -0400

    Added LAb 5 Files

---
+++ LAB MATERIALS/LAB 5 PT1/app.py
@@ -0,0 +1,32 @@
+import time
+import requests
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+
+@app.route('/weather')
+def get_weather():
+    api_key = "93a15b1668a3dbe03afe5a5fbf693237"
+    city = "Chicago"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url)
+    return response.json()
